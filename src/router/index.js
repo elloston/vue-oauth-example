0bad9aec6a1c00159fda15eb5a9add5c9c90e5a9
elloston <elloston@icloud.com>
2024-04-12 12:34:00 +0700

    Updated base path

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import routes from "./routes";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
+  base: "/oauthexample",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
